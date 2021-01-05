e17b65e22e10ae4aeb5e876bd3dde192c3480dff
Elif Eryıldırım <elifery06@yandex.com>
2021-01-05 21:58:06 +0300

    first commit

---
+++ partialview/Controllers/UrunController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using partialview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace partialview.Controllers
+{
+    public class UrunController : Controller
+    {
+        public IActionResult Index()
+        {
+            Urun urun = new Urun();
+            urun.UrunAdi = "kalem";
+            urun.UrunAdedi = 5;
+            urun.UrunKodu = 5647;
+            return View(urun);
+        }
+        //[HttpGet]
+        //public IActionResult UrunEkle()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        public IActionResult UrunEkle(Urun product)
+        {
+            Urun urun = new Urun();
+            urun.UrunAdi = product.UrunAdi;
+            urun.UrunAdedi = product.UrunAdedi;
+            urun.UrunKodu = product.UrunKodu;
+            return View(urun);
+        }
+    }
+}
---
+++ partialview/Models/Urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace partialview.Models
+{
+    public class Urun
+    {
+        [Required]
+        public string UrunAdi { get; set; }
+        public int UrunKodu { get; set; }
+        public int  UrunAdedi { get; set; }
+    }
+}
